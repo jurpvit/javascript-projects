89e375ace9d5072bd58ca90728f869c427a0adbc
Vitalii Moudrak <101230862@georgebrown.ca>
2022-02-20 21:33:11 -0500

    Update script.js

---
+++ Javascript Projects/Countdown Timer/script.js
@@ -0,0 +1,7 @@
+/*
+Todo looping 4 season videos for background based on weather API
+Beautify app frontend
+Make custom input and a countdown timer for it
+Make standardized Holiday countdown timer based on button clicks.
+Make birthday background changer based on Countdown
+*/
