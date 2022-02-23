c8d00670c3d1e7612f60f7a399866d2787ef02f5
Vitalii Moudrak <101230862@georgebrown.ca>
2022-02-23 16:10:28 -0500

    added features
    sidemenu, social icons, changed background.

--- Javascript Projects/Countdown Timer/script.js
+++ Javascript Projects/Countdown Timer/script.js
@@ -5,3 +5,12 @@ Make custom input and a countdown timer for it
 Make standardized Holiday countdown timer based on button clicks.
 Make birthday background changer based on Countdown
 */
+
+
+const menuToggle = document.querySelector('.toggle')
+const showcase = document.querySelector('.showcase')
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active')
+    showcase.classList.toggle('active')
+})
